a339d77d837d43c978eafa3dd617c9e418286afe
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-03 00:08:29 -0500

    add first beginner challenge Not Sum Ten

---
+++ script.py
@@ -0,0 +1,19 @@
+
+#Control Flow Challenges (Beginner)
+
+'''
+Challenge IB
+You are given two numbers stored in num1 and num2. 
+If the sum of num1 and num2 is NOT equal to 10, then store True into a variable called not_ten, 
+otherwise store False in not_ten.
+
+'''
+num1 = 6
+num2 = 3
+
+if num1 + num2 != 10:
+  not_ten = True
+else:
+  not_ten = False
+
+print("Is the sum of the numbers not equal to 10? " + str(not_ten))
