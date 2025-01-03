8ffe6e6c51ff0535069b51f8aeacf55886640c1e
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-03 00:31:34 -0500

    update organization of files

--- Beginner/over_budget.py
+++ Beginner/over_budget.py
@@ -1,25 +1,4 @@
-
 #Control Flow Challenges (Beginner)
-
-'''
-Challenge 1
-You are given two numbers stored in num1 and num2. 
-If the sum of num1 and num2 is NOT equal to 10, then store True into a variable called not_ten, 
-otherwise store False in not_ten.
-
-'''
-
-#Challenge 1: Not Sum Ten
-
-num1 = 6
-num2 = 3
-
-if num1 + num2 != 10:
-  not_ten = True
-else:
-  not_ten = False
-
-print("Is the sum of the numbers not equal to 10? " + str(not_ten))
 
 '''
 Challenge 2
