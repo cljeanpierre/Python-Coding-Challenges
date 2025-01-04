9ca345ff8bb737e82024770321eb6c9ead3a5828
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-03 23:50:38 -0500

    write the instructions for the first advanced challenge of Control Flow Challenges called In Range

---
+++ Advanced/in_range.py
@@ -0,0 +1,10 @@
+#Control Flow Challenges (Advanced)
+
+'''
+Challenge 
+Test whether a number falls within a certain range. First, accept three parameters where the first parameter 
+is the number being tested, the second parameter is the lower bound and the third parameter is the upper bound 
+of the range. 
+'''
+
+#In Range
