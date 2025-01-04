4a8f0516d04e2b5af03f68fceb35ff1575c8886d
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 00:48:33 -0500

    write the function for the advanced control loop function called Movie Review and test it.

--- Advanced/movie_review.py
+++ Advanced/movie_review.py
@@ -9,3 +9,15 @@ and tell the user how the movie was based on the rating.
 '''
 
 #Movie Review
+
+def movie_review(rating):
+  if rating <= 5:
+    return "Avoid at all costs!"
+  if rating > 5 and rating < 9:
+    return "This one was fun."
+  return "Outstanding!" 
+
+
+print(movie_review(9))
+print(movie_review(4))
+print(movie_review(6))
