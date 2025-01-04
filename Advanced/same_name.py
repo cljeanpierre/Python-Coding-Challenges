c999f36ba0ab34d344f7aab64225ab173bc77da6
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 00:11:25 -0500

    write the function for Same Name and test it

--- Advanced/same_name.py
+++ Advanced/same_name.py
@@ -4,7 +4,15 @@
 Challenge 
 Write a program that checks different names and determines if they are equal. Accept two strings and 
 compare them.
- 
+
 '''
 
 #Same Name
+
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  return False
+
+print(same_name("Colby", "Colby"))
+print(same_name("Tina", "Amber"))
