6ff1519c6f50fd284178910e859a6d0c270ccbe0
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 00:30:33 -0500

    write the instructions for the advanced control flow challenge called Movie Review.

---
+++ Advanced/movie_review.py
@@ -0,0 +1,11 @@
+#Control Flow Challenges (Advanced)
+
+'''
+Challenge
+
+Create a function that will rate movies. The function will split the ratings into different ranges 
+and tell the user how the movie was based on the rating. 
+
+'''
+
+#Movie Review
