8176f36b4fed5eadf295f48aad888409c24b0216
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 00:07:46 -0500

    write the instructions for an advanced control flow challenge called Same Name

---
+++ Advanced/same_name.py
@@ -0,0 +1,10 @@
+#Control Flow Challenges (Advanced)
+
+'''
+Challenge 
+Write a program that checks different names and determines if they are equal. Accept two strings and 
+compare them.
+ 
+'''
+
+#Same Name
