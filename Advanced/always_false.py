44c59eab21fc203efa8437309d477e2c56390135
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 00:18:44 -0500

    write instructions for the advanced control flow challenge called Always False.

---
+++ Advanced/always_false.py
@@ -0,0 +1,9 @@
+#Control Flow Challenges (Advanced)
+
+'''
+Challenge 
+Create a function that contains a contradition, an occurrence when your condition will always be false 
+no matter what value you pass into it. 
+'''
+
+#Always False
