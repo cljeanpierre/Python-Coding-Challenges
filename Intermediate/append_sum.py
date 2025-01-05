2c7eb3857eb31182e32c86617c809658ff26bc0d
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-04 21:41:27 -0500

    add the instructions for the intermediate challenge called Append Size

---
+++ Intermediate/append_sum.py
@@ -0,0 +1,10 @@
+#List Challenges (Intermediate)
+
+'''
+Challenge 
+Create a function that calculates the sum of the last two elements of an input list and appends it to the end 
+of the original list. After doing so, it repeats this process two more times and returns the resulting list.
+
+'''
+
+#Append Size
