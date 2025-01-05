b466e7d9398de1700fbc8b738b6964d44d284425
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-05 14:32:48 -0500

    write the instructions for Intermediate List challenge More Than N

---
+++ Intermediate/more_than_n.py
@@ -0,0 +1,10 @@
+#List Challenges (Intermediate)
+
+'''
+Challenge 
+A factory produces a variety of different flavored snacks. The different types of snacks are represented by 
+their id and are kept on a conveyor belt. Check if there are enough items of a certain snack in inventory.
+
+'''
+
+# More Than N
