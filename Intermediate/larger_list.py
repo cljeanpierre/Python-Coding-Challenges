6c2027390eecff0beca30d38430df02c000cecec
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-05 14:10:25 -0500

    write the instructions for the Intermediate List Challenge called Larger List

---
+++ Intermediate/larger_list.py
@@ -0,0 +1,12 @@
+#List Challenges (Intermediate)
+
+'''
+Challenge 
+You are working with two conveyor belts that contain items represented by a numerical ID. If one conveyor belt 
+contains more items than the other, return the ID of the last item on that belt. In the case where the belts 
+have the same number of items, return the last item from the first conveyor belt.
+
+'''
+
+#Larger List
+
