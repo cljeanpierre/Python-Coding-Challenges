722ec8de82f03cf8a85d422ea5ac8917c1f9c166
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-06 14:09:08 -0500

    add the instructions and file for the Intermediate List Challenge Combine Sort

---
+++ Intermediate/combine_sort.py
@@ -0,0 +1,10 @@
+#List Challenges (Intermediate)
+
+'''
+Challenge 
+Create a function that combines two different lists together and then sorts them. To do this, combine the lists 
+with an operation and then sort using a function call.
+
+'''
+
+#Combine Sort
