e6121bf4a573cb7277dc9930187268cf916ed054
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-06 14:27:52 -0500

    write the function for the Intermediate List challenge Combine Sort

--- Intermediate/combine_sort.py
+++ Intermediate/combine_sort.py
@@ -8,3 +8,10 @@ with an operation and then sort using a function call.
 '''
 
 #Combine Sort
+
+def combine_sort(my_list1, my_list2):
+  onelist = my_list1 + my_list2
+  onelist.sort()
+  return onelist
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
