c24160a7cf291a0e329339dcdb879f3b8cd9e123
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-06 21:33:44 -0500

    add function for Intermediate Loop challenge Delete Starting Even Numbers

--- Intermediate/delete_starting_even_nums.py
+++ Intermediate/delete_starting_even_nums.py
@@ -9,3 +9,11 @@ numbers at the beginning of the list are removed.
 '''
 
 #Delete Starting Even Numbers
+
+def delete_starting_evens(my_list):
+    while len(my_list) != 0 and my_list[0] % 2 == 0:  
+      my_list.pop(0)
+    return my_list
+
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
