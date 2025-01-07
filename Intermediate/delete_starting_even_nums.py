9c5abb6aa4631d1876c8a93cef7e0ae69cdcb7e2
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-06 20:45:56 -0500

    add instructions for the Intermediate Loops challenge Delete Starting Even Nums

---
+++ Intermediate/delete_starting_even_nums.py
@@ -0,0 +1,11 @@
+#Loops Challenges (Intermediate)
+
+'''
+Challenge 
+Create a function that will repeatedly remove the first element of a list until it finds an odd number or runs 
+out of elements. Accept a list of numbers as an input parameter and return the modified list where any even 
+numbers at the beginning of the list are removed.
+
+'''
+
+#Delete Starting Even Numbers
