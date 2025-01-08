1a7632a955821489b72c4fcc4ac6e12d5ad6dbf1
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-08 18:42:06 -0500

    add instructions and functions for the advanced Loops challenge Same Values.

---
+++ Advanced/Loops Challenges/same_values.py
@@ -0,0 +1,19 @@
+#Loops Challenges (Advanced)
+
+'''
+Find the indices in two equally sized lists where the numbers match. Iterate through both of them at the same
+time and compare the values. If the numbers are equal, then record the index. 
+
+'''
+
+#Same Values
+
+def same_values(list1, list2):
+  match = []
+  for index in range(len(list1)):
+    if list1[index] == list2[index]:
+      match.append(index)
+  return match
+
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
