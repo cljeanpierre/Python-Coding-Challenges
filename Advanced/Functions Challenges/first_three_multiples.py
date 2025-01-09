57206f31ce07ab0501cf8f3e8f8b21d021dcf9bd
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-08 23:58:17 -0500

    add instructions for the advanced Functions challenge First Three Multiples

---
+++ Advanced/Functions Challenges/first_three_multiples.py
@@ -0,0 +1,11 @@
+#Functions Challenges (Advanced)
+
+'''
+Challenge
+
+Create a function which both prints and returns values. For this function, print out the first three multiples 
+of a number and return the third multiple. 
+
+'''
+
+#First Three Multiples
