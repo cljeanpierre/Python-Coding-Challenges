fd320b1b340df13a48da12fc6d184fb8d12563e5
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-08 22:47:52 -0500

    add instructions, function, and math solutions for the beginner functions challenge Tenth Power.

---
+++ Beginner/tenth_power.py
@@ -0,0 +1,21 @@
+#Function Challenges (Beginner)
+
+'''
+Challenge 
+Create some functions which can help solve math problems! For this first function, take the tenth power of a 
+number. 
+
+'''
+
+#Tenth Power
+
+def tenth_power(num):
+  result = num ** 10
+  return result
+
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
