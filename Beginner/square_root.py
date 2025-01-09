62c4bc2483947ecdd05d8bc64085710e5ba09ce7
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-08 22:58:17 -0500

    add instructions, function, and math solutions for the beginner functions challenge Square Root.

---
+++ Beginner/square_root.py
@@ -0,0 +1,18 @@
+#Function Challenges (Beginner)
+
+'''
+Challenge 
+Create some functions which can help solve math problems! For this first function, take the square root of a 
+number. 
+
+'''
+
+#Square Root
+
+def square_root(num):
+  return num ** 0.5
+
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
