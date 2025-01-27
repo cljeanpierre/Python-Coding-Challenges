1a9c58c018e3e732023827559c68dacc0703cd5b
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-27 13:57:48 -0500

    add instructions for the intermediate challenge Count Letters

---
+++ Intermediate/String Challenges/count_letters.py
@@ -0,0 +1,11 @@
+#String Challenges (Intermediate)
+
+'''
+Challenge 
+Count the number of unique letters in a string. In other words, any new letters should be counted and any 
+duplicates should not be counted. There are a few ways to solve this, but we will use the provided alphabet 
+string to ensure that duplicates are not counted. 
+
+'''
+
+#Count Letters
