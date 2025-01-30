f64f807586a5217e876ef8d171efaa9db0e81423
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-29 21:32:18 -0500

    define a function that counts the number of occurrences of a given letter in any word

--- Intermediate/String Challenges/count_x.py
+++ Intermediate/String Challenges/count_x.py
@@ -9,3 +9,14 @@ and the character 's' would result in an answer of 4.
 '''
 
 #Count X
+
+def count_char_x(word, x):
+  num_of_x = 0
+  for letter in word:
+    if letter == x:
+      num_of_x += 1
+  return num_of_x
+
+print(count_char_x("Mosiah", "z"))
+print(count_char_x("Obi", "i"))
+print(count_char_x("Mississippi", "s"))
