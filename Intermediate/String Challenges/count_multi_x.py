928aa7a8308aa11d2c5f35efabf25ebeb6330616
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-29 21:45:56 -0500

    write the instructions for the intermediate String challenge Count Multi X

---
+++ Intermediate/String Challenges/count_multi_x.py
@@ -0,0 +1,11 @@
+#String Challenges (Intermediate)
+
+'''
+Challenge 
+Unlike our function in count_x, we are going to compare against an entire string instead of a single character. 
+This function should accept a string and a substring to compare against. Return the number of occurrences of
+second parameter within the first parameter.  In other words, we are checking the number of occurrences of the 
+shorter string (second parameter) within the longer string (first parameter). 
+'''
+
+#Count Multi X
