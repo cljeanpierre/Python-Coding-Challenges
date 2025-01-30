6b0cdfda50b4bdf81f21dcc5f854f7eceaa8bfdd
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-29 21:16:17 -0500

    create a list of letters

--- Intermediate/String Challenges/count_letters.py
+++ Intermediate/String Challenges/count_letters.py
@@ -4,8 +4,11 @@
 Challenge 
 Count the number of unique letters in a string. In other words, any new letters should be counted and any 
 duplicates should not be counted. There are a few ways to solve this, but we will use the provided alphabet 
-string to ensure that duplicates are not counted. 
+string to ensure that duplicates are not counted. Uppercase and lowercase letters should be counted as
+different letters
 
 '''
 
 #Count Letters
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
