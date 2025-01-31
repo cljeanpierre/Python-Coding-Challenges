c716ecefe46e3bddbc849b7b1fcbbff60835e316
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-01-30 22:47:55 -0500

    add instructions for Substring Between intermediate string challenge

---
+++ Intermediate/String Challenges/substring_between.py
@@ -0,0 +1,11 @@
+#String Challenges (Intermediate)
+
+'''
+Challenge 
+Create a function that is able to extract a portion of a string between two characters. The function will take 
+three parameters where the first parameter is the string we are going to extract the substring from, the second 
+input is the starting character of our substring and the third character is the ending character of our 
+substring. 
+'''
+
+#Substring Between
