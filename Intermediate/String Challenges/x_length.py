a06a27c9fb6cd336cf371a56e77ce2adfb112c02
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 00:24:19 -0500

    add instructions for intermediate string challenge called x length

---
+++ Intermediate/String Challenges/x_length.py
@@ -0,0 +1,10 @@
+#String Challenges (Intermediate)
+
+'''
+Challenge 
+Using the split method in a different way, create a new function that is able to accept two inputs: one for a 
+sentence and another for a number. The function returns True if every single word in the sentence has a length 
+greater than or equal to the number provided. 
+'''
+
+#X Length
