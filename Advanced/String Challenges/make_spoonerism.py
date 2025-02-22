58505b4138fb20bfa1f508bab311de4c958975dc
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 18:05:18 -0500

    write a function that fulfills the objectives of the advanced String Challenge Make Spoonerism

--- Advanced/String Challenges/make_spoonerism.py
+++ Advanced/String Challenges/make_spoonerism.py
@@ -8,3 +8,17 @@ similar, but instead of using the first syllable, we are going to switch the fir
 '''
 
 #Make Spoonerism
+def make_spoonerism(word1, word2):
+  first_char1 = word1[0] 
+  first_char2 = word2[0]
+  remain_char1 = word1[1:]
+  remain_char2 = word2[1:]
+  spooned = first_char2 + remain_char1 + " " + first_char1 + remain_char2
+  return spooned
+
+print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+print(make_spoonerism("a", "b"))
+# should print b a
