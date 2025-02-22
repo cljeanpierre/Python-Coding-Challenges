d9b4c25f38e9585276fcb4d3f640c482c238b219
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 17:33:17 -0500

    write objectives for the advanced String challenge Reverse

---
+++ Advanced/String Challenges/reverse.py
@@ -0,0 +1,10 @@
+#String Challenges (Advanced)
+
+'''
+Challenge 
+This one is similar to the Every Other Letter advanced String challenge. Instead of selecting every other letter,
+we want to reverse the entire string. This can be performed in a similar manner, but we will need to modify the 
+range we are using.
+'''
+
+#Reverse
