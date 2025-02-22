e42a698bed24aeba4d4cd65999a7926882e236d0
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 17:29:31 -0500

    write a function that fulfills the objective of Every Other Letter advanced String challenge

--- Advanced/String Challenges/every_other_letter.py
+++ Advanced/String Challenges/every_other_letter.py
@@ -6,3 +6,15 @@ Create a function that extracts every other letter from a string and returns the
 '''
 
 #Every Other Letter
+
+def every_other_letter(word):
+  new_string = ""
+  for i in range(0, len(word), 2):
+    new_string += word[i]
+  return new_string
+
+
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
