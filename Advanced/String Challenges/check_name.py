6297d453e65745b7a1e435f9686c5afd99bad8ab
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 00:40:15 -0500

    add instructions for the advanced string challenge Name Check

---
+++ Advanced/String Challenges/check_name.py
@@ -0,0 +1,10 @@
+#String Challenges (Advanced)
+
+'''
+Challenge 
+You are creating an app that allows users to interact and share their coding project ideas online. The first 
+step is to provide your name in the application and a greeting for other people to see which contains your name. 
+Create a function that is able to check if a user’s name is located within their greeting.
+'''
+
+#Name Check
