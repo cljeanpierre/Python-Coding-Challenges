f4e9cf02a76e243e40d3283d3bf60fa8ed0fae43
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 18:10:47 -0500

    write the instructions for the advanced String challenge Add Exclamation

---
+++ Advanced/String Challenges/add_exclamation.py
@@ -0,0 +1,11 @@
+#String Challenges (Advanced)
+
+'''
+Challenge 
+Let’s say we are writing a program that displays a large message on a blimp and we need to fill in any missing
+space if a short phrase is provided. Our function will accept a string and if the size is less than 20, it will
+fill in the remaining space with exclamation marks until the size reaches 20. If the provided string already 
+has a length greater than 20, then we will simply return the original string.
+'''
+
+#Add Exclamation
