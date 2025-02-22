c260fafaacb40557860f9e2339fa8e07b64743fe
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 17:53:01 -0500

    write the objectives for the advanced String challenge Make Spoonerism

---
+++ Advanced/String Challenges/make_spoonerism.py
@@ -0,0 +1,10 @@
+#String Challenges (Advanced)
+
+'''
+Challenge 
+A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism
+is made when someone says “Belly Jeans” instead of “Jelly Beans”. We are going to make a function that is 
+similar, but instead of using the first syllable, we are going to switch the first character of two words. 
+'''
+
+#Make Spoonerism
