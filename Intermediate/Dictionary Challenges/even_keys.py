0d8b68b9e507051912a1f5360209992b68ec6242
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 22:58:03 -0500

    add the instructions for the intermediate Dictionary Challenge Even Keys

---
+++ Intermediate/Dictionary Challenges/even_keys.py
@@ -0,0 +1,9 @@
+#Dictionary Challenges (Intermediate)
+
+'''
+Challenge 
+Similar to Sum Values, but we are going to use the keys in order to retrieve the values. Additionally, we are
+going to only look at every even key within the dictionary. 
+'''
+
+#Even Keys
