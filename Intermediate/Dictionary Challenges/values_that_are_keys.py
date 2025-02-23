982e9671545c5706d5a02fe071a99268a003e29f
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 23:23:19 -0500

    add the instructions for the intermediate Dictionary Challenge Values that are Keys

---
+++ Intermediate/Dictionary Challenges/values_that_are_keys.py
@@ -0,0 +1,11 @@
+#Dictionary Challenges (Intermediate)
+
+'''
+Challenge 
+We are making a program that will create a family tree. Using a dictionary, we want to return a list of all 
+the children who are also parents of other children. Using dictionaries we can consider those people to be 
+values which are also keys in our dictionary of family data.
+
+'''
+
+#Values That Are Keys
