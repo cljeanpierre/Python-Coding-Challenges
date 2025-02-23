bfd23aa871ddab85224d306f737c1d2fd935e2c7
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 23:40:20 -0500

    add a function that meets the objective of Largest Value, the intermediate dictionary challenge

--- Intermediate/Dictionary Challenges/largest_value.py
+++ Intermediate/Dictionary Challenges/largest_value.py
@@ -8,3 +8,18 @@ is a twist on the max algorithm since it is using a dictionary rather than a lis
 '''
 
 #Largest Value
+
+def max_key(my_dictionary):
+  start_key = 0
+  start_val = 0
+  for key, value in my_dictionary.items():
+    if value > start_val:
+      start_val = value
+      start_key = key
+  return start_key
+
+
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
