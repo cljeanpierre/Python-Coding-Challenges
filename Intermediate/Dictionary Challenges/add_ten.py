0d211a43fec65a4e7928501481c4291fd64cba32
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 23:10:04 -0500

    add the instructions for the intermediate Dictionary coding challenge Add Ten

---
+++ Intermediate/Dictionary Challenges/add_ten.py
@@ -0,0 +1,11 @@
+#Dictionary Challenges (Intermediate)
+
+'''
+Challenge 
+Similar to Even Keys, we are going to loop through the keys again, but this time we are going to modify the 
+values within the dictionary. Our function should add 10 to every value in the dictionary and return the 
+modified dictionary. 
+
+'''
+
+#Add Ten
