3aaf39c295948f19d667c90d4a9d4d9f66443097
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 22:48:10 -0500

    add the instructions for the intermediate Dictionary Challenge Sum Values

---
+++ Intermediate/Dictionary Challenges/sum_values.py
@@ -0,0 +1,9 @@
+#Dictionary Challenges (Intermediate)
+
+'''
+Challenge 
+For this coding challenge, we are going to look at only the values in a dictionary. This function should sum up
+all of the values from the key-value pairs in the dictionary.
+'''
+
+#Sum Values
