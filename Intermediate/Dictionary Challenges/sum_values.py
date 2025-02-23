20d862c8e6ac909f327607b44cf1d984396d844f
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-22 22:55:31 -0500

    write a function that fulfills the objectives of the intermediate Dictionary challenge Sum Values

--- Intermediate/Dictionary Challenges/sum_values.py
+++ Intermediate/Dictionary Challenges/sum_values.py
@@ -7,3 +7,15 @@ all of the values from the key-value pairs in the dictionary.
 '''
 
 #Sum Values
+
+def sum_values(my_dictionary):
+  sum_vals = 0
+  for vals in my_dictionary.values():
+    sum_vals += vals
+  return sum_vals
+  
+
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
