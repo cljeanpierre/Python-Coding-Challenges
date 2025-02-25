6a7908ab026c13e1acb768ed0f8108662b7fadfe
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-24 23:40:11 -0500

    add function that meets requirements of the advanced Dictionary Challenge Frequency Count

--- Advanced/Dictionary Challenges/frequency_count.py
+++ Advanced/Dictionary Challenges/frequency_count.py
@@ -9,3 +9,16 @@ strings as input and return a new dictionary.
 '''
 
 #Frequency Count
+
+def frequency_dictionary(words):
+  word_frequency = {}
+  for word in words:
+    if word not in word_frequency:
+      word_frequency[word] = 0
+    word_frequency[word] += 1
+  return word_frequency
+
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
