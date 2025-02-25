05397683b78538ec890939e68fdbe12f03bd1808
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-24 23:17:22 -0500

    add a function that meets the objectives of the advanced Dictionary Challenge Word Length

--- Advanced/Dictionary Challenges/word_length_dict.py
+++ Advanced/Dictionary Challenges/word_length_dict.py
@@ -8,3 +8,14 @@ every string in the list of strings, while the values will be the length of each
 '''
 
 #Word Length Dictionary
+
+def word_length_dictionary(words):
+  word_lengths = {}
+  for word in words:
+    word_lengths[word] = len(word)
+  return word_lengths
+
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
