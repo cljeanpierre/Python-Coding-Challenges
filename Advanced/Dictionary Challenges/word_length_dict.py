f67d03c9d780db0de42dfe352512ddc695d3ed63
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-24 23:13:10 -0500

    add instructions for the advanced dictionary challenge Word Length Dictionary

---
+++ Advanced/Dictionary Challenges/word_length_dict.py
@@ -0,0 +1,10 @@
+#Dictionary Challenges (Advanced)
+
+'''
+Challenge 
+Write a function that creates a new dictionary based on a list of strings. The keys of the dictionary will be 
+every string in the list of strings, while the values will be the length of each of the words in the string list. 
+
+'''
+
+#Word Length Dictionary
