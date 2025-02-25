238bd3bad75e9f957efb65210c6c7efd677cfe71
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-02-24 23:20:13 -0500

    add instructions for the advanced Dictionary Challenge Frequency Count

---
+++ Advanced/Dictionary Challenges/frequency_count.py
@@ -0,0 +1,11 @@
+#Dictionary Challenges (Advanced)
+
+'''
+Challenge 
+This function is similar to Word Length Dictionary, but instead of counting the length of each string in the 
+list of strings, we will be counting the frequency of each string. This function will also accept a list of 
+strings as input and return a new dictionary. 
+
+'''
+
+#Frequency Count
