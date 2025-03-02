6fcad59398d4daf95d1657b2995e9b1d30895ad8
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 15:28:12 -0500

    add the function that meets the requirements of advanced dictionary challenge Unique Values

--- Advanced/Dictionary Challenges/unique_values.py
+++ Advanced/Dictionary Challenges/unique_values.py
@@ -8,3 +8,15 @@ number which is the count of all values from the dictionary without including du
 '''
 
 #Unique Values
+
+def unique_values(my_dictionary):
+  new_dict = []
+  for value in my_dictionary.values():
+    if value not in new_dict:
+      new_dict.append(value)
+  return len(new_dict)
+
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
