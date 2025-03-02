a83f10a2912b6a0275afaf68de5e0813971cff4d
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 15:59:13 -0500

    add instructions and function that meets requirements for adv dictionary challenge Count First Letter

---
+++ Advanced/Dictionary Challenges/count_first_letter.py
@@ -0,0 +1,25 @@
+#Dictionary Challenges (Advanced)
+
+'''
+Challenge 
+Create a function that accepts a dictionary where the keys are last names and the values are lists of first 
+names of people who have that last name. Calculate the number of people who have the same first letter in their 
+last name. 
+
+'''
+
+#Count First Letter
+
+def count_first_letter(names):
+  letters = {}
+  for key in names:
+    first_letter = key[0]
+    if first_letter not in letters:
+      letters[first_letter] = 0
+    letters[first_letter] += len(names[key])
+  return letters
+
+
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
