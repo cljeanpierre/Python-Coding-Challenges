f9e13a26c4cd189d62640ca0fb8d9b18926b4b5c
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 18:27:57 -0500

    add instructions for setting up a robot in the first of five sequence intermediate class challenges

---
+++ Advanced/Classes Challenges/setting_up_robot.py
@@ -0,0 +1,16 @@
+#Classes Challenges (Advanced)
+
+'''
+Challenge 
+Use your programming knowledge to create a new robotics company. Your idea for micro driving robots which are 
+able to pick up and deliver objects was promising and now you want to start programming the logic. The Classes 
+coding challenges will use your knowledge of Classes to solve some example scenarios. 
+
+Try solving the first of these five challenges below!
+
+First, create the class for our robot. Begin by setting up the instance variables to keep track of important 
+data related to the robot. 
+
+'''
+
+#Setting Up Our Robot
