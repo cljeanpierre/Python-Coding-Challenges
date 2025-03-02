ccfef6bb78a43f0e3607ba9ee806f31b8cadc801
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 18:55:32 -0500

    add instructions for robot logic in advanced Class Challenges

---
+++ Advanced/Classes Challenges/add.robot_logic.py
@@ -0,0 +1,17 @@
+#Classes Challenges (Advanced)
+
+'''
+Challenge 
+Use your programming knowledge to create a new robotics company. Your idea for micro driving robots which are 
+able to pick up and deliver objects was promising and now you want to start programming the logic. The Classes 
+coding challenges will use your knowledge of Classes to solve some example scenarios. 
+
+Try solving the second of these five challenges below!
+
+Add some logic to our robot. It would be very useful to be able to control our robot, so we are going to create 
+a control_bot method which changes the speed and direction. We are also going to create a method called 
+adjust_sensor. This method is used to change the range of our robot’s sensors which are used to detect obstacles. 
+
+'''
+
+#Adding Robot Logic
