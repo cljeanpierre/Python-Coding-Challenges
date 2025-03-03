bee8d17d0d33b4e772008e2de835ef71d3d78c7a
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 21:29:42 -0500

    add instructions to identify the robots by id in the advanced Classes challenge for robots

---
+++ Advanced/Classes Challenges/identify_robots.py
@@ -0,0 +1,18 @@
+#Classes Challenges (Advanced)
+
+'''
+Challenge 
+Use your programming knowledge to create a new robotics company. Your idea for micro driving robots which are 
+able to pick up and deliver objects was promising and now you want to start programming the logic. The Classes 
+coding challenges will use your knowledge of Classes to solve some example scenarios. 
+
+Try solving the last of these five challenges below!
+
+In order to keep track of the robots we are creating, we want to be able to assign an ID value to each robot 
+when it is created. If we create five robots in a row, we want the IDs of each robot to be 1, 2, 3, 4, and 5 
+respectively. This can be accomplished by using a class variable counter which increments and is assigned to 
+an instance variable for the ID whenever the constructor is called. 
+
+'''
+
+#Identifying Robots
