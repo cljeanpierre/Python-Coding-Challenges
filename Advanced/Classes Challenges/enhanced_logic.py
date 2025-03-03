a0dcd76826877517bf2d8049454a07642307e227
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2025-03-02 19:52:09 -0500

    add instructions for third part of robot Class challenge, Enhanced Logic

---
+++ Advanced/Classes Challenges/enhanced_logic.py
@@ -0,0 +1,17 @@
+#Classes Challenges (Advanced)
+
+'''
+Challenge 
+Use your programming knowledge to create a new robotics company. Your idea for micro driving robots which are 
+able to pick up and deliver objects was promising and now you want to start programming the logic. The Classes 
+coding challenges will use your knowledge of Classes to solve some example scenarios. 
+
+Try solving the third of these five challenges below!
+
+It can be tedious manually setting the values for each instance variable after we have created an object from 
+the DriveBot class. We can enhance our constructor to automatically assign the values we provide to the 
+instance variables. Instead of taking zero parameters, we are going to make the constructor take three 
+parameters.
+'''
+
+#Enhanced Logic
